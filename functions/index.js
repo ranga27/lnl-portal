5d1c21a8eab2841d76892070cc76943e32ffd37f
ranga27 <sarang.radke@gmail.com>
2022-09-16 01:15:44 +0100

    organised functions as monorepo in firebase.json

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,5 @@ const sendVerificationEmail = require("./users/sendVerificationEmail");
 const confirmEmail = require("./users/confirmEmail");
 admin.initializeApp();
 
-// Grouping fucntions to do targetted partial deployment. This will help separating the Root SSR Function from the Functions in the /functions folder.
-exports.emailGroup = {
-  sendVerificationEmail: sendVerificationEmail.sendVerificationEmail,
-  confirmEmail: confirmEmail.confirmEmail,
-};
+exports.sendVerificationEmail = sendVerificationEmail.sendVerificationEmail;
+exports.confirmEmail = confirmEmail.confirmEmail;
