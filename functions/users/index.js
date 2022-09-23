7d4e85c1a9384d0e4a24d59e9eabcfb28ad9faea
Abraham_Kol <Abrahamkolawole87@gmail.com>
2022-09-23 10:27:52 +0100

    plat 235 - confirmation pages

--- functions/users/index.js
+++ functions/users/index.js
@@ -1,7 +1,6 @@
 const sendVerificationEmail = require('./sendVerificationEmail');
 const confirmEmail = require('./confirmEmail');
 const sendonboardingemail = require('./onboardingEmail');
-const cors = require('cors')({ origin: true });
 
 exports.sendVerificationEmail = sendVerificationEmail.sendVerificationEmail;
 exports.confirmemail = confirmEmail.confirmemail;
---
+++ src/pages/email-confirmation/success.jsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+
+const SuccessMessage = () => (
+  <div>
+    <section className='h-screen max-w-2xl mx-auto'>
+      <div className='py-48 xl:py-64 h-full text-gray-800'>
+        <div className='flex xl:justify-center lg:justify-center justify-center items-center flex-wrap'>
+          <div className='w-48 h-48 relative text-center mx-auto'>
+            <Image
+              src='/assets/white.png'
+              alt='Loop Not Luck'
+              layout='fill'
+              className='w-full'
+              objectFit='cover'
+            />
+          </div>
+          <div>
+            <p className='text-center text-3xl font-extrabold text-gray-900 mb-8'>
+              Email Confirmed!
+            </p>
+
+            <h4 className='text-center font-semibold mb-0'>
+              Your email address has been confirmed successfully
+              <br />
+              please{' '}
+              <Link href='/login'>
+                <a className='text-black underline hover:text-[#F7B919] focus:text-[#F7B919] transition duration-200 ease-in-out'>
+                  login
+                </a>
+              </Link>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+);
+
+export default SuccessMessage;
