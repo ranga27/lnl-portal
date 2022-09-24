2e597748ff9e937594cd6716eff154c697d83b71
ranga27 <sarang.radke@gmail.com>
2022-09-24 01:34:00 +0100

    diabling eslint for prod builds

--- src/next.config.js
+++ src/next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //distribution directory path is required for the SSR function
-  distDir: '../.next',
+  distDir: "../.next",
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['images.unsplash.com', 'lh3.googleusercontent.com'],
+    domains: ["images.unsplash.com", "lh3.googleusercontent.com"],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 module.exports = nextConfig;
