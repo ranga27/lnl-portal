e7b911c67fbd3c17d15153fc037493b46041d7f2
Abraham_Kol <Abrahamkolawole87@gmail.com>
2022-09-26 15:43:15 +0100

    PLAT 241 - add pricing ui

---
+++ src/components/data/pricing.js
@@ -0,0 +1,49 @@
+// eslint-disable-next-line import/prefer-default-export
+export const pricing = [
+  {
+    name: 'Single Post',
+    priceMonthly: 500,
+    includedFeatures: ['1 role credit.', 'Unlimited hires.', 'Single Payment.'],
+  },
+  {
+    name: 'Starter',
+    priceMonthly: 188,
+    priceYearly: 2250,
+    includedFeatures: [
+      '5 role credits.',
+      'Unlimited hires.',
+      'Save 10%.',
+      '12 month subscription.',
+      'Interest free payments.',
+    ],
+  },
+  {
+    name: 'Pro',
+    priceMonthly: 354,
+    priceYearly: 4250,
+    includedFeatures: [
+      '10 role credits.',
+      'Unlimited hires.',
+      'Save 15%.',
+      '12 month subscription.',
+      'Interest free payments.',
+      'Talent team training.',
+      'Dedicated account manager.',
+    ],
+  },
+  {
+    name: 'Enterprise',
+    priceMonthly: 688,
+    priceYearly: 8250,
+    includedFeatures: [
+      '20 role credits.',
+      'Unlimited hires.',
+      'Save 17.5%.',
+      '12 month subscription.',
+      'Interest free payments.',
+      'Talent team training.',
+      'Dedicated account manager.',
+      'Promotion across social media channels.',
+    ],
+  },
+];
