28891c7faf20be3a6d713d0091c7bfaa7c27de70
developerdesigner18 <developer.designer18@gmail.com>
2022-09-28 14:52:49 +0530

    added partial form saving feature

--- src/pages/onboarding/index.js
+++ src/pages/onboarding/index.js
@@ -1,16 +1,15 @@
-import { useEffect, useContext, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import { useRouter } from 'next/router';
 import StepWizard from 'react-step-wizard';
-import { collection, doc } from 'firebase/firestore';
-import { useFirestoreDocumentData } from '@react-query-firebase/firestore';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
 import Step4 from './Step4';
 import Stepper from './Stepper';
 import { AuthContext } from '../../components/context/AuthContext';
-import { firestore } from '../../../firebase/clientApp';
 import Dashboard from '../dashboard';
+import useDocument from '../../components/hooks/useDocument';
+import useCollection from '../../components/hooks/useCollection';
 
 const Onboarding = () => {
   const router = useRouter();
@@ -29,42 +28,21 @@ const Onboarding = () => {
     return null;
   }
 
-  const [fields, setFields] = useState({
-    diversity: null,
-    industry: null,
-    companyValues: null,
-    location: '',
-    companyLocation: '',
-    companyName: '',
-    jobRole: '',
-  });
-
-  const updateForm = (values) => {
-    const newFields = { ...fields, ...values };
-    setFields(newFields);
-  };
   const onStepChange = () => {
     // console.log(stats);
   };
 
-  const collectionRef = collection(firestore, 'companyUsers');
-  const ref = doc(collectionRef, userId);
-  const { isLoading, data: user } = useFirestoreDocumentData(
-    ['companyUsers'],
-    ref,
-    {
-      subscribe: true,
-    },
-    {
-      onSuccess() {
-        console.debug('User Data loaded successfully');
-      },
-      onError(error) {
-        console.error('Woops, something went wrong!', error);
-      },
-    }
+  const { isLoading: isUserLoading, data: user } = useDocument(
+    'companyUsers',
+    userId
   );
-  if (isLoading) {
+
+  const { isLoading: isCompanyLoading, data: company } = useCollection(
+    'companyV2',
+    ['userId', '==', userId]
+  );
+
+  if (isUserLoading && isCompanyLoading) {
     return <div className='loading' />;
   }
 
@@ -80,10 +58,10 @@ const Onboarding = () => {
               isHashEnabled
               nav={<Stepper />}
             >
-              <Step1 hashKey={'FirstStep'} update={updateForm} />
-              <Step2 update={updateForm} />
+              <Step1 hashKey={'FirstStep'} user={user} />
+              <Step2 userId={userId} company={company[0]} />
               <Step3 />
-              <Step4 hashKey={'TheEnd!'} fields={fields} />
+              <Step4 hashKey={'TheEnd!'} user={user} company={company[0]} />
             </StepWizard>
           </div>
         </div>
