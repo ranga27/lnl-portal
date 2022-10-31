d4866d14bdc9f21b307e19a0c7411988630b9d7c
Abraham_Kol <Abrahamkolawole87@gmail.com>
2022-10-31 09:20:22 +0100

    change to use firebase secrets

--- functions/users/sendVerificationEmail.js
+++ functions/users/sendVerificationEmail.js
@@ -1,20 +1,33 @@
 // Firestore Triggers are not yet available in V2
-const functions = require("firebase-functions/v1");
-const { sendEmail } = require("./sendEmail");
+const functions = require('firebase-functions/v1');
+const { sendEmail } = require('./sendEmail');
+const nodemailer = require('nodemailer');
 
 exports.sendVerificationEmail = functions
-  .region("europe-west2")
-  .firestore.document("temporaryCompanyUsers/{uid}")
+  .region('europe-west2')
+  .runWith({ secrets: ['NODEMAILER_AUTH_PASSWORD'] })
+  .firestore.document('temporaryCompanyUsers/{uid}')
   .onCreate(async (snapshot) => {
     const tempUserInfo = snapshot.data();
     const { email, confirmationHash, firstName } = tempUserInfo;
 
+    const transporter = nodemailer.createTransport({
+      host: process.env.NODEMAILER_HOST_URL,
+      port: process.env.NODEMAILER_PORT,
+      secure: true,
+      auth: {
+        user: process.env.NODEMAILER_AUTH_USER,
+        pass: process.env.NODEMAILER_AUTH_PASSWORD,
+      },
+    });
+
     return sendEmail({
       to: email,
-      from: "Loop Not Luck hello@loopnotluck.com",
-      subject: "Click here to verify your email address - Get in the Loop",
+      from: 'Loop Not Luck hello@loopnotluck.com',
+      subject: 'Click here to verify your email address - Get in the Loop',
       firstName,
       // TODO: change URL to reflect the portal URLs. Ideally this shouldn't be hardcoded
       message: `${process.env.CONFIRM_URL}?conf=${confirmationHash}`,
+      transporter,
     });
   });
