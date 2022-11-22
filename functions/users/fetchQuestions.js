dcd92809bbe1f360e765dee49c5364e5ceb75ec0
developerdesigner18 <developer.designer18@gmail.com>
2022-11-22 17:47:25 +0530

    enabled cors

--- functions/users/fetchQuestions.js
+++ functions/users/fetchQuestions.js
@@ -1,25 +1,28 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions/v1');
+const cors = require('cors')({ origin: true });
 
 exports.fetchQuestions = functions
   .region('europe-west2')
   .https.onRequest(async (req, res) => {
-    const { companyId } = req.query;
+    cors(req, res, async () => {
+      const { companyId } = req.query;
 
-    const store = admin.firestore();
+      const store = admin.firestore();
 
-    let questions = [];
+      let questions = [];
 
-    await store
-      .collection('questionnaire')
-      .where('companyId', '==', companyId)
-      .get()
-      .then((docs) => {
-        docs.forEach((doc) => {
-          questions.push({ ...doc.data(), id: doc.id });
-          questions = doc.data().questions;
+      await store
+        .collection('questionnaire')
+        .where('companyId', '==', companyId)
+        .get()
+        .then((docs) => {
+          docs.forEach((doc) => {
+            questions.push({ ...doc.data(), id: doc.id });
+            questions = doc.data().questions;
+          });
         });
-      });
 
-    res.status(200).json(questions);
+      return res.status(200).json(questions);
+    });
   });
