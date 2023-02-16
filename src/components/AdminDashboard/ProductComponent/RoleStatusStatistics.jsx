edf74cdf12126a0f0816880711b7dff28eb1ed73
developerdesigner18 <developer.designer18@gmail.com>
2023-02-16 12:54:32 +0530

    Add Role Statitics table on product Statistics page

--- src/components/AdminDashboard/ProductComponent/RoleStatusStatistics.jsx
+++ src/components/AdminDashboard/ProductComponent/RoleStatusStatistics.jsx
@@ -1,17 +1,102 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getSavedAppliedStatistics } from '../../../utils/getSavedAppliedStatistics';
+import { format } from 'date-fns';
 
 const RoleStatusStatistics = () => {
-  useEffect(() => {
-    getSavedAppliedStatistics()
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const [roleStatistics, setRoleStatistics] = useState([]);
+  const tableColums = [
+    'ID',
+    'Title',
+    'Company',
+    'Saved By',
+    'Applied By',
+    'Average Match %',
+    'Salary',
+    'Deadline',
+  ];
+
+  useEffect(async () => {
+    const data = await getSavedAppliedStatistics();
+    setRoleStatistics(data);
   }, []);
-  return <div>Hello</div>;
+
+  const average = (arr) => arr.reduce((p, c) => p + c, 0) / arr.length;
+
+  return (
+    <>
+      <div className='max-w-[100%] rounded shadow-lg p-5'>
+        <h1 className='p-2 py-4'>
+          Role Statistics :{' '}
+          {roleStatistics.length <= 0
+            ? 'Please Wait we are preparing Data for you...'
+            : ''}
+        </h1>
+        <div className='max-h-[250px] overflow-auto'>
+          <table className='table-auto w-full'>
+            <thead className='text-xs font-semibold uppercase text-gray-400 bg-gray-50 sticky top-0 p-0'>
+              <tr>
+                {tableColums.map((col) => (
+                  <th className='p-2'>
+                    <div className='font-semibold text-left'>{col}</div>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className='text-sm divide-y divide-gray-200'>
+              {roleStatistics?.length &&
+                roleStatistics.map((role, index) => (
+                  <tr>
+                    <td className='p-2'>
+                      <div className='font-medium text-gray-800'>{index}</div>
+                    </td>
+                    <td className='p-2'>
+                      <div className='font-medium text-gray-800'>
+                        {`${role?.title}`}
+                      </div>
+                    </td>
+                    <td className='p-2'>
+                      <div className='font-medium text-gray-800'>
+                        {role?.company ? role.company : 'NA'}
+                      </div>
+                    </td>
+                    <td className='p-2'>
+                      <div className='font-medium text-gray-800'>
+                        {role?.saved ? role?.saved : 0}
+                      </div>
+                    </td>
+                    <td className='p-2'>
+                      <div className='font-medium text-gray-800'>
+                        {role?.applied ? role?.applied : 0}
+                      </div>
+                    </td>
+                    <td className='p-2'>
+                      <div className='font-medium text-gray-800'>
+                        {role?.score ? average(role.score).toFixed(2) : 0}
+                      </div>
+                    </td>
+                    <td className='p-2'>
+                      <div className='font-medium text-gray-800'>
+                        {role?.salary ? role.salary : 'N/A'}
+                      </div>
+                    </td>
+                    <td className='p-2'>
+                      <div className='font-medium text-gray-800'>
+                        {role?.deadline
+                          ? format(
+                              new Date(role.deadline.toDate()),
+                              'dd-MMM-yyyy'
+                            )
+                          : 'No Date Found'}
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RoleStatusStatistics;
--- src/utils/getSavedAppliedStatistics.js
+++ src/utils/getSavedAppliedStatistics.js
@@ -1,42 +1,50 @@
 import { collection, getDocs } from 'firebase/firestore';
 import { firestore } from '../../firebase/clientApp';
 
-export const getSavedAppliedStatistics = () => {
-  return new Promise(async (resolve) => {
-    let statistics = [];
-    const users = collection(firestore, 'users');
-    const snapshot = await getDocs(users);
+export const getSavedAppliedStatistics = async () => {
+  const usersRef = collection(firestore, 'users');
+  const usersSnapshot = await getDocs(usersRef);
 
-    snapshot.forEach(async (user) => {
-      const roleRef = collection(firestore, `users/${user.id}/matchedRoles`);
-      const querySnapshot = await getDocs(roleRef);
-      querySnapshot.forEach((role) => {
-        let roleData = role.data();
-        let added = false;
-        statistics.map((stats) => {
-          if (stats.id === roleData.id) {
-            if (roleData.saved) {
-              stats.saved = stats.saved + 1;
-            }
-            if (roleData.applied) {
-              stats.applied = stats.applied + 1;
-            }
-            added = true;
-          }
+  const statistics = [];
+  let totalApplied = 0;
+  let totalSaved = 0;
+
+  for (const userDoc of usersSnapshot.docs) {
+    console.log('Function Called');
+    const matchedRolesRef = collection(
+      firestore,
+      `users/${userDoc.id}/matchedRoles`
+    );
+    const matchedRolesSnapshot = await getDocs(matchedRolesRef);
+
+    for (const roleDoc of matchedRolesSnapshot.docs) {
+      const roleData = roleDoc.data();
+      const existingStats = statistics.find((stats) => stats.id === roleDoc.id);
+
+      if (existingStats) {
+        existingStats.saved += roleData.saved ? 1 : 0;
+        existingStats.applied += roleData.applied ? 1 : 0;
+        existingStats.score.push(roleData.score);
+      } else {
+        statistics.push({
+          id: roleDoc.id,
+          title: roleData.title,
+          company: roleData.company,
+          saved: roleData.saved ? 1 : 0,
+          applied: roleData.applied ? 1 : 0,
+          salary: roleData.salary,
+          deadline: roleData.deadline,
+          score: [roleData.score],
         });
-        if (!added) {
-          statistics.push({
-            id: role.id,
-            title: roleData.title,
-            company: roleData.company,
-            saved: roleData.saved ? 1 : 0,
-            applied: roleData.applied ? 1 : 0,
-            salary: roleData.salary,
-            deadline: roleData.deadline,
-          });
-        }
-      });
-      if (statistics.length) resolve(statistics);
-    });
-  });
+      }
+
+      if (roleData.applied) {
+        totalApplied++;
+      }
+      if (roleData.saved) {
+        totalSaved++;
+      }
+    }
+  }
+  if (statistics.length) return statistics;
 };
