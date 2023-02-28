0bc38420d75561f9b08c6512d442cb8803dfb9ba
Abraham_Kol <Abrahamkolawole87@gmail.com>
2023-02-28 12:45:16 +0000

    Fix emulator to send emails

--- functions/users/index.js
+++ functions/users/index.js
@@ -2,9 +2,15 @@ const sendCompanyOnboardingEmail = require('./onboardingEmail');
 const updateInviteCredits = require('./updateInviteCredits');
 const fetchQuestions = require('./fetchQuestions');
 const applicantResults = require('./applicantResults');
+// const {
+  // sendCompanyVerificationEmail,
+// } = require('./sendCompanyVerificationEmail');
+// const { companyEmailConfirmation } = require('./companyEmailConfirmation');
 
 exports.sendCompanyOnboardingEmail =
   sendCompanyOnboardingEmail.sendCompanyOnboardingEmail;
 exports.updateInviteCredits = updateInviteCredits.updateInviteCredits;
 exports.fetchQuestions = fetchQuestions.fetchQuestions;
 exports.applicantResults = applicantResults.applicantResults;
+// exports.sendCompanyVerificationEmail = sendCompanyVerificationEmail;
+// exports.companyEmailConfirmation = companyEmailConfirmation;
